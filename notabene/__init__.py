e282f27d55597619bff386ea7f7037aa6ac2c825
Barret Rennie <barret@brennie.ca>
2014-10-03 10:34:12 -0600

    Add copyright info

--- notabene/__init__.py
+++ notabene/__init__.py
@@ -1,5 +1,5 @@
-# {one line to give the program's name and a brief idea of what it does.}
-# Copyright (C) {year}  {name of author}
+# Notabene -- Build sets of notes with PDFLatex.
+# Copyright (C) 2014  Barret Rennie
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
